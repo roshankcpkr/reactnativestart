f7cd0b1122bcd5708533d7f5e8fcb1729a944975
Roshan KC <roshankcpkr123@gmail.com>
2023-03-26 19:28:58 +0545

    styles and states are same as react

--- App.js
+++ App.js
@@ -1,30 +1,69 @@
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+  function goalInputHandler(enteredText)
+  {
+    setEnteredText(enteredText);
+  }
+  function addGoalHandler()
+  {
+    setCourseGoals((currentCourseGoals)=>{
+      return [...currentCourseGoals, enteredText];
+    })
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.inputBar} placeholder="Your goals!" />
-        <Button title="Add Goal" />
+        <TextInput onChangeText={goalInputHandler} style={styles.inputBar} placeholder="Your goals!" />
+        <Button onPress={addGoalHandler} title="Add Goal" />
       </View>
-      <View><Text>List of goals</Text></View>
+      <View style={styles.goalsContainer}>{courseGoals.map(el=>{
+        return<View key={el} style={styles.goalItem}>
+          <Text style={styles.goalText} key={el}>{el}</Text>
+          </View>
+        
+      })}</View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex:1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   inputBar: {
     borderWidth: 1,
     borderColor: '#cccccc',
     padding: 4,
-    width: '80%',
+    width: '70%',
     marginRight: 8,
+
+  },
+  goalsContainer: {
+    flex: 5
+  },
+  goalItem: {
+    padding: 8,
+    margin: 8,
+    backgroundColor: '#5e0acc',
+    borderRadius: 6,
+  },
+  goalText: {
+    color: 'white',
   }
 });
